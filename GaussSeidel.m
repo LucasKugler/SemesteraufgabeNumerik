50cf9957da23aa5f85e8baab4038165b8db86b10
LucasKugler <63842022+LucasKugler@users.noreply.github.com>
2021-01-05 00:13:53 +0100

    Gauss Seidel implementiert

--- GaussSeidel.m
+++ GaussSeidel.m
@@ -17,12 +17,37 @@ iter=0;
 fprintf('-------------------------------------------- \n');
 fprintf('Starting Gauss-Seidel Iteration!    \n');
 
+uTilde = zeros(n,1);
+
 while delta>eps
    iter=iter+1; 
+   uTilde = zeros(n,1);
 
-   %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-   %Hier muss etwas implementiert werden%
-   %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+   for i = 1:n
+       
+       for j = 1:n
+           if j < i
+               uTilde(i) = uTilde(i) + A(i,j)*uTilde(j);
+           elseif j > i
+               uTilde(i) = uTilde(i) + A(i,j)*U(j);
+           end
+       end
+    %    for j = 1:(i-1)
+    %         uTilde(i) = uTilde(i) + A(i,j)*uTilde(j);
+    %    end
+
+    %    for j = (i+1):n
+    %         uTilde(i) = uTilde(i) + A(i,j)*U(j);
+    %    end
+
+       uTilde(i) = - (uTilde(i) - B(i)) / A(i,i);
+   end
+   
+   U = (1 - omega)*U + omega*uTilde;
+   
+   R = B - A*U;
+   delta = norm(R);
+   
 end
 fprintf('Gauss-Seidel Iteration done. Number of iterations:%i    \n',iter);
 
